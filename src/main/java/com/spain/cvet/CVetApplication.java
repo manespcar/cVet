36612c6c7aac4bc23684e800241036a4c5f7d2ec
Manuel Jesús España Carnicer <mjespana@atsistemas.com>
2018-03-09 11:12:27 +0100

    first commit

---
+++ src/main/java/com/spain/cvet/CVetApplication.java
@@ -0,0 +1,12 @@
+package com.spain.cvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CVetApplication.class, args);
+	}
+}
